0e34028e5db7a2575fe811e56cdac54b75661c44
troygilman0 <32723159+TheTGKing@users.noreply.github.com>
2025-03-31 17:56:51 -0400

    fixed redirect

--- ui/root_templ.go
+++ ui/root_templ.go
@@ -91,7 +91,7 @@ func (view RootView) View() templ.Component {
 			}
 			return nil
 		})
-		templ_7745c5c3_Err = gong.NewLink("/docs/").Render(templ.WithChildren(ctx, templ_7745c5c3_Var3), templ_7745c5c3_Buffer)
+		templ_7745c5c3_Err = gong.NewLink("/").Render(templ.WithChildren(ctx, templ_7745c5c3_Var3), templ_7745c5c3_Buffer)
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
